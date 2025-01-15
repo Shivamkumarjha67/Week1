6b36da8d376022ab7b0d5c17f37c9386a4fec6f9
Shivam Kumar Jha <shkrjh@gmail.com>
2025-01-15 23:14:55 +0530

    Changes made

---
+++ DAY1/Level2/DivisibleByFive.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DivisibleByFive {
+	public static void main(String[] args) {
+		// Initializing the scanner object
+		Scanner input = new Scanner(System.in);
+
+		// Taking input of number
+		System.out.println("Enter the number : ");
+		int num = input.nextInt();
+
+		// conversion into pound
+		boolean result = num % 5;
+
+		System.out.println("Is the number " + num + " divisible by 5? : " + result); 
+
+		// closing the scanner resource
+		input.close();
+	} 
+}
